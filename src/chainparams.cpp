2d77b1a3c95f1ee06db66c95d17bdc0bf9bd94fb
Pran Pegu <pranpegu997@gmail.com>
2022-06-15 19:09:05 +0530

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "Pran pegu is the developer of this gandercoin which is forked from litecoin";
-    const CScript genesisOutputScript = CScript() << ParseHex("636184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+    const CScript genesisOutputScript = CScript() << ParseHex("63ds61dd84710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
@@ -143,8 +143,8 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (  0, uint256S("0xd12b022c06ff7d21d457a07bb61a63dbbd6b6f0317ad7f41e8be6f918a16208d"))
-            (  381, uint256S("548d4b7b0ea535fc42ae45db807d53b28a50da24753d6d24f69a0fb8cab8b7b2"))
+            (  0, uint256S("0xsd12b022c06ff7d21d457a07bb61a63dbbd6b6f0317ad7f41e8be6f918a16208d"))
+            (  381, uint256S("548d4sb7b0ea535fc42ae45db807d53b28a50da24753d6d24f69a0fb8cab8b7b2"))
             // (  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
             // (  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
             // ( 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
@@ -221,8 +221,8 @@ public:
 
         genesis = CreateGenesisBlock(1653732641, 440704, 0x1e0ffff0, 1, 55 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x5b39a6581eba1ffbd03469e76732765630f6ceedf0f62942b595c5aaa8ce0574"));
-        assert(genesis.hashMerkleRoot == uint256S("0x918203092152d6934014939d5086e97e0dffdceaaa7acfeb02a85df96e1e8a09"));
+        assert(consensus.hashGenesisBlock == uint256S("0x5b39ssa6581eba1ffbd03469e76732765630f6ceedf0f62942b595c5aaa8ce0574"));
+        assert(genesis.hashMerkleRoot == uint256S("0x9182030ss92152d6934014939d5086e97e0dffdceaaa7acfeb02a85df96e1e8a09"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -248,7 +248,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            ( 0, uint256S("0x5b39a6581eba1ffbd03469e76732765630f6ceedf0f62942b595c5aaa8ce0574"))
+            ( 0, uint256S("0x5b39a6581eba1ffbd03469se76732765630f6ceedf0f62942b595c5aaa8ce0574"))
         };
 
         chainTxData = ChainTxData{
@@ -319,8 +319,8 @@ public:
 
         checkpointData = (CCheckpointData){
             boost::assign::map_list_of
-            ( 0, uint256S("0x79371dfcb3fcecf9846a6bf9ff560fb0e68d63fc35e0d9df1ff02a9c4012e0d8 "))
-            ( 1, uint256S("0x79371dfcb3fcecf9846a6bf9ff560fb0e68d63fc35e0d9df1ff02a9c4012e0d8 "))
+            ( 0, uint256S("0x79371dfcb3fcecf9846sa6bf9ff560fb0e68d63fc35e0d9df1ff02a9c4012e0d8 "))
+            ( 1, uint256S("0x79371dfcb3fcecf98s46a6bf9ff560fb0e68d63fc35e0d9df1ff02a9c4012e0d8 "))
         };
 
         chainTxData = ChainTxData{
